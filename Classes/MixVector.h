2485a4627c43f286349c1189fe7fc84b6c937698
jakob <jakob@rfc-1918.(none)>
2010-10-22 16:10:48 +0200

    completed implementation of Matrix

--- Classes/MixVector.h
+++ Classes/MixVector.h
@@ -7,10 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "Matrix.h"
 
 @interface MixVector : NSObject {
-
+	float _x;
+	float _y;
+	float _z;
 }
+@property (nonatomic) float x,y,z;
+
++(MixVector*) initWithX: (float)x y:(float) y z:(float) z;
+-(void) addVectorX: (float) x y:(float) y z: (float) z;
+-(void) addVector: (MixVector*) vector;
+-(void) subX: (float) x y:(float) y z: (float) z;
+-(void) subVector: (MixVector*) vector;
+-(void) multVecotr: (MixVector*) vector;
+-(void) multWithScalar: (float) s;
+-(void) divideVectorWithScale: (float) s ;
+-(void) length;
+-(void) norm;
+-(void) crossWithVectorA: (MixVector*) u VectorB: (MixVector*) v;
+-(void) setVector: (MixVector*) vector;
+//-(void) prodWithMat: (Matrix*) m;
 
 @end
+
+
