b80e8bbd54ac34b0429fcc849d7d2e8c04a08d48
jakob <jakob@rfc-1918.(none)>
2010-10-25 14:10:38 +0200

    Added Circle View for the marker

---
+++ Classes/Circle.h
@@ -0,0 +1,16 @@
+//
+//  Circle.h
+//  Mixare
+//
+//  Created by jakob on 25.10.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Circle : UIView {
+
+}
+
+@end
---
+++ Classes/Circle.m
@@ -0,0 +1,38 @@
+//
+//  Circle.m
+//  Mixare
+//
+//  Created by jakob on 25.10.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Circle.h"
+
+
+@implementation Circle
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        [self setBackgroundColor:[UIColor clearColor]];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+- (void)drawRect:(CGRect)rect {
+	//Get the CGContext from this view
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	// stroke and fill black with a 0.5 alpha
+	CGContextSetRGBStrokeColor(context, 0.0, 0.0, 255.0, 1.0);
+	CGContextSetLineWidth(context, 2.0);
+	CGContextSetPatternPhase(context, CGSizeMake(2, 2));
+	CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 0.5);
+	CGContextStrokeEllipseInRect(context, rect);
+	// now draw the circle
+	CGContextFillEllipseInRect (context, rect);
+}
+
+@end
