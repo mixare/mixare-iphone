5b7794b19f44c2537ebc03f3df8aa4139a1ca183
eXaLy <aswin.ly@gmail.com>
2012-10-18 10:40:24 +0200

    Twitter JSON convert test

---
+++ UnitTests/TwitterProcessorTest.h
@@ -0,0 +1,15 @@
+//
+//  TwitterProcessorTest.h
+//  Mixare
+//
+//  Created by Aswin Ly on 18-10-12.
+//  Copyright (c) 2012 Peer GmbH. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "DataProcessor.h"
+#import "TwitterProcessor.h"
+
+@interface TwitterProcessorTest : SenTestCase
+
+@end
