c11a635607aa1a15969efadc3f0f3796733ab5ae
eXaLy <aswin.ly@gmail.com>
2012-11-30 16:37:40 +0100

    Replaced files

---
+++ Classes/gui/tabbarControllers/augmentedView/MarkerView.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010- Peer internet solutions
+ * 
+ * This file is part of mixare.
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+#import <UIKit/UIKit.h>
+
+//Info view of a poi. Shoved when tapping on a por in the cam view. opens a webview of the poi
+@interface MarkerView : UIView {
+    UIView *viewTouched;
+    NSString *_url;
+    UIView *loadView;
+    BOOL webActivated;
+}
+
+@property (nonatomic, strong) UIView *viewTouched;
+@property (nonatomic, strong) NSString *url;
+
+@end
