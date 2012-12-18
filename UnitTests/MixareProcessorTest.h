50d828d11751271d6b8120ce567e3e4d333bbeaf
eXaLy <aswin.ly@gmail.com>
2012-12-18 11:48:09 +0100

    Added some unit tests

---
+++ UnitTests/MixareProcessorTest.h
@@ -0,0 +1,14 @@
+//
+//  MixareProcessorTest.h
+//  Mixare
+//
+//  Created by Aswin Ly on 18-12-12.
+//  Copyright (c) 2012 Peer GmbH. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "MixareProcessor.h"
+
+@interface MixareProcessorTest : SenTestCase
+
+@end
